0e432b7bb24c7088e6e0d4dedf623559f7ae0d89
Jeremy Philipson <jeremyphilipson@gmail.com>
2019-03-11 20:44:46 -0400

    prettier file

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express')
+
+const app = express()
+
+const schema = gql`
+	type Query {
+		me: User
+	}
+
+	type User {
+		username: String!
+	}
+`
+const resolvers = {
+	Query: {
+		me: () => ({ username: 'jeremyphilipson' })
+	}
+}
+
+const server = new ApolloServer({
+	typeDefs: schema,
+	resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen({ port: 8000 }, () => console.log(`listening on port 8000/graphql`))
